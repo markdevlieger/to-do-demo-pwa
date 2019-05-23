6ada5a6ae1db08f0e01cf91b460445107950563a
Mark de Vlieger <mark.de.vlieger@newitera.nl>
2019-05-23 13:37:36 +0000

    Added service worker

---
+++ webapp/register-worker.js
@@ -0,0 +1,18 @@
+if ("serviceWorker" in navigator) {
+	window.addEventListener("load", function () { // eslint-disable-line sap-forbidden-window-property
+		navigator.serviceWorker // eslint-disable-line sap-no-navigator
+			.register("./service-worker.js")
+			.then(function() {
+				console.log("Service worker registered"); // eslint-disable-line no-console
+			})
+			.catch(function (err) {
+				console.log("Service Worker Failed to Register", err); // eslint-disable-line no-console
+			});
+		var refreshing;
+		navigator.serviceWorker.addEventListener("controllerchange", function () { // eslint-disable-line sap-no-navigator
+			if (refreshing) {return;}
+			window.location.reload(); // eslint-disable-line sap-no-location-reload
+			refreshing = true;
+		});
+	});
+}
